71e799c04ccf3a73dba2be01c4fddfcaabbf7a70
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-24 19:08:44 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
 import React, {useState} from 'react';
-import Form from './component/Form.js';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./component/Form";
 
 
-  function App() {
-    const [
-      members, 
-      setMembers] = 
-      useState({ name: "", email: "", role: "" });
-  
-    const teamMembers = event => {
-      setMembers({ ...members, [event.target.name]: event.target.value });
-    };
-  
-    const handleSubmit = event => {
-      event.preventDefault();
-      console.log(member.name);
-      console.log(member.email);
-      console.log(member.role);
-    };
-  
-    return (
-      <div className="movie-list">
-        {members.map(form)};
-      </div>
-    );
-    
-    return (
-      <div className="App">
-        {console.log(member)}
-        <form onSubmit={event => handleSubmit(event)}>
-          <label>
-            name:
-            <input
-              type="text"
-              name="name"
-              value={member.name}
-              onChange={event => handleChange(event)}
-            />
-          </label>
-          <label>
-            email:
-            <input
-              type="text"
-              name="email"
-              value={member.email}
-              onChange={event => handleChange(event)}
-            />
-          </label>
-          <label>
-          <input>
-              type="text"
-              name="role"
-              value={member.role}
-              onChange={event => handleChange(event)}
-            />
-          </label>
-          <button>Submit!</button>
-        </form>
-      </div>
-          <button>Submit!</button>
-        </form>
-      </div>
-    );
-  }
+function App() {
+  // eslint-disable-next-line
+  const [member, setMember] = useState([]);
+  console.log(member);
+  return (
+    <div className="App">
+      
+       <h1>Teams</h1>
+       {member.map(person => (
+         <Form key={person.info} info={person} />
+       ))}
+       <Form />
+       
+    </div>
+  );
+}
 
-  export default App;
- 
+export default App;
--- src/component/Form.js
+++ src/component/Form.js
@@ -1,4 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
 
 
-export default Form;
+export default function Form () {
+    const [member, setMember] = useState({name: "", email: "", role: ""});
+
+    function handleChange(event) {
+        const updatedMember = {...member, [event.target.name]: event.target.value};
+        console.log("handleChange", event.target.name, event.target.value, updatedMember);
+        setMember(updatedMember);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("user state", member);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Name:</label>
+                <input
+                  type="name"
+                  className="form-group"
+                  name="name"
+                  placeholder="Enter your Name"
+                  onChange={handleChange}
+                  value={member.name} />
+
+            </div>
+            <div className="form-group">
+                <label>Email:</label>
+                <input
+                type="email"
+                className="form-group"
+                name="email"
+                placeholder="Enter email"
+                onChange={handleChange}
+                value={member.email}
+                />
+                
+            </div>
+            <div className="form-group">
+                <label>Role:</label>
+                <input
+                type="role"
+                className="form-group"
+                name="role"
+                placeholder="Enter your role"
+                onChange={handleChange}
+                value={member.role}
+                />
+                
+            </div>
+            <button type="submit" className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+}

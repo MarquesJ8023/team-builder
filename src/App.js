900ae797506c4b619280e6b5e9bcdcfccf24b049
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-24 20:05:09 -0400

    commit commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
-import './App.css';
-import Form from "./component/Form";
+import {group} from './component/Data.js';
+import  './App.css';
+import Form from "./component/Form.js";
 
 
 function App() {
   // eslint-disable-next-line
-  const [member, setMember] = useState([]);
-  console.log(member);
+  const [members, setMembers] = useState(group);
+  console.log(members);
   return (
     <div className="App">
       
        <h1>Teams</h1>
-       {member.map(person => (
-         <Form key={person.info} info={person} />
+       {members.map((team, index) => (
+         <Form key={index} team={group} />
        ))}
        <Form />
        
---
+++ src/component/Data.js
@@ -0,0 +1,27 @@
+export const group = [ 
+    {
+      name: 'Dan',
+      email: '',
+      role: 'Instructor',
+    },
+    {
+      name: 'Me',
+      email: 'marques.johnson1992@Gmail.com',
+      role: 'Full Stack Web Developer',
+      
+    },
+    {
+      name: 'HomeGirl',
+      email: '',
+      role: 'Full Stack Web Developer',
+      
+    },
+    {
+      name: 'Shelton',
+      email: '',
+      role: 'Team Lead',
+      
+    },
+  
+    
+  ];
